1af1c8648522acd0b5345bd5141da0b5b5bcff61
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-09-18 16:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,3 +26,41 @@ for (let i = 0; i < squares; i++) {
 
     container.appendChild(square);
 }
+
+() => {
+    cy.visit(baseUrl + "/main.html");
+
+    // Ensure the container and squares exist
+    cy.get(".container").should("exist");
+    cy.get(".container").find(".square").eq(799).should("exist");
+
+    // Check initial background color of the first square
+    cy.get(".square").first().should("have.css", "background-color", "rgb(29, 29, 29)");
+
+    // Trigger mouseover and check background color change
+    cy.get(".square").first().trigger("mouseover");
+    cy.wait(1000); // Wait to ensure hover effect is applied
+    cy.get(".square").first().should("not.have.css", "background-color", "rgb(29, 29, 29)");
+
+    // Trigger mouseout and check background color returns to normal
+    cy.get(".square").first().trigger("mouseout");
+    cy.wait(1000); // Wait to ensure the hover effect is removed
+    cy.get(".square").first().should("have.css", "background-color", "rgb(29, 29, 29)");
+
+    // Additional test cases for multiple squares
+    cy.get(".square").eq(100).trigger("mouseover");
+    cy.get(".square").eq(101).trigger("mouseover");
+    cy.wait(1000); // Wait to ensure hover effect is applied to both squares
+
+    // Check if colors of the hovered squares have changed
+    cy.get(".square").eq(100).should("not.have.css", "background-color", "rgb(29, 29, 29)");
+    cy.get(".square").eq(101).should("not.have.css", "background-color", "rgb(29, 29, 29)");
+
+    // Trigger mouseout and check if the colors return to normal
+    cy.get(".square").eq(100).trigger("mouseout");
+    cy.get(".square").eq(101).trigger("mouseout");
+    cy.wait(1000); // Wait to ensure hover effect is removed
+    cy.get(".square").eq(100).should("have.css", "background-color", "rgb(29, 29, 29)");
+    cy.get(".square").eq(101).should("have.css", "background-color", "rgb(29, 29, 29)");
+}
+
